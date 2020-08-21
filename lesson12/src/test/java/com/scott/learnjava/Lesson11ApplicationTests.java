3ed62f8cc6c1933c15eb419b1dcf7a9c93ccedd7
Scott Blanch <scottglenblanch@gmail.com>
2020-08-20 22:58:20 -0700

    Thu Aug 20 22:58:20 MST 2020

---
+++ lesson12/src/test/java/com/scott/learnjava/Lesson11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scott.learnjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
